1ee2694f7d81904dd49a189e80e50b224f101ef2
lily (๑◡๑) <ilovesuno@proton.me>
2025-09-03 22:04:25 -0800

    Suspending Windows support, for now

--- scripts/ScramJetInstaller.sh
+++ scripts/ScramJetInstaller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 set -euxo pipefail
+
 isWin() {
     case "$OSTYPE" in
         msys*|cygwin*|win32*) return 0 ;;
@@ -9,43 +9,32 @@ isWin() {
 }
 
 cd ScramJet
-
 if isWin; then
-    # powershell -NoProfile -Command "
-    #     Set-StrictMode -Version Latest
-    #     \$ErrorActionPreference = 'Stop'
-    #     \$cargoRoot = Join-Path \$env:USERPROFILE '.cargo'
-    #     \$cargoBin = Join-Path \$cargoRoot 'bin'
-    #     \$localLib = Join-Path \$cargoRoot 'lib'
-    #     Invoke-WebRequest -Uri 'https://win.rustup.rs/x86_64' -OutFile 'rustup-init.exe'
-    #     Start-Process -FilePath '.\\rustup-init.exe' -ArgumentList '-y' -NoNewWindow -Wait
-    #     Remove-Item '.\\rustup-init.exe' -Force
-    #     if (-not (Test-Path \$cargoBin)) { New-Item -ItemType Directory -Path \$cargoBin -Force | Out-Null }
-    #     if (-not (Test-Path \$localLib)) { New-Item -ItemType Directory -Path \$localLib -Force | Out-Null }
-    #     \$env:Path += ';' + \$cargoBin
-    #     pnpm i
-    #     rustup install stable
-    #     rustup default stable
-    #     & rustup run stable cargo install wasm-bindgen-cli --force
-    #     \$releaseUrl = 'https://github.com/WebAssembly/binaryen/releases/latest'
-    #     \$response = Invoke-WebRequest -Uri \$releaseUrl -MaximumRedirection 0 -ErrorAction SilentlyContinue
-    #     \$version = (\$response.Headers.Location -split '/')[-1]
-    #     \$archiveName = \"binaryen-\$version-x86_64-windows.tar.gz\"
-    #     \$downloadUrl = \"https://github.com/WebAssembly/binaryen/releases/download/\$version/\$archiveName\"
-    #     Invoke-WebRequest -Uri \$downloadUrl -OutFile \$archiveName
-    #     tar -xvf \$archiveName
-    #     Remove-Item \$archiveName -Force
-    #     Get-ChildItem \"binaryen-\$version\\bin\" | ForEach-Object {
-    #         Copy-Item \$_.FullName \$cargoBin -Force
-    #     }
-    #     \$libFile = \"binaryen-\$version\\lib\"
-    #     if ((Test-Path \$libFile) -and ((Get-Item \$libFile).PSIsContainer -eq \$false)) {
-    #         Copy-Item \$libFile \$localLib -Force
-    #     }
-    #     Remove-Item \"binaryen-\$version\" -Recurse -Force
-    #     cargo install --git https://github.com/r58playz/wasm-snip --force
-    # "
-    echo "Unfortunately, Windows is not yet supported.. To use Revision, use a Linux environment."
+    powershell -NoProfile -Command "
+        \$ErrorActionPreference = 'Stop'
+        if (-not (Get-Command rustup -ErrorAction SilentlyContinue)) {
+            Invoke-WebRequest -Uri 'https://win.rustup.rs/x86_64' -OutFile 'rustup-init.exe'
+            Start-Process -FilePath '.\\rustup-init.exe' -ArgumentList '-y' -NoNewWindow -Wait
+            Remove-Item '.\\rustup-init.exe' -Force
+        }
+        rustup toolchain install nightly-x86_64-pc-windows-gnu
+        rustup target add wasm32-unknown-unknown --toolchain nightly-x86_64-pc-windows-gnu
+        & rustup run nightly-x86_64-pc-windows-gnu cargo install wasm-bindgen-cli --force
+        \$releaseUrl = 'https://github.com/WebAssembly/binaryen/releases/latest'
+        \$response = Invoke-WebRequest -Uri \$releaseUrl -MaximumRedirection 0 -ErrorAction SilentlyContinue
+        \$version = (\$response.Headers.Location -split '/')[-1]
+        \$archiveName = \"binaryen-\$version-x86_64-windows.tar.gz\"
+        \$downloadUrl = \"https://github.com/WebAssembly/binaryen/releases/download/\$version/\$archiveName\"
+        Invoke-WebRequest -Uri \$downloadUrl -OutFile \$archiveName
+        tar -xvf \$archiveName
+        Remove-Item \$archiveName -Force
+        Copy-Item \"binaryen-\$version\\bin\\*\" \$env:USERPROFILE\\.cargo\\bin -Force
+        Remove-Item \"binaryen-\$version\" -Recurse -Force
+        & rustup run nightly-x86_64-pc-windows-gnu cargo install --git https://github.com/r58playz/wasm-snip --force
+    "
+    pnpm i
+    rustup run nightly-x86_64-pc-windows-gnu bash ./rewriter/wasm/build.sh
+    pnpm build
 else
     curl --proto "=https" --tlsv1.2 --sSf https://sh.rustup.rs | sh
     source "$HOME/.cargo/env"
@@ -59,7 +48,6 @@ else
     mv binaryen-${Version}/lib/* ~/.local/lib
     rm -rf binaryen-${Version}
     cargo install --git https://github.com/r58playz/wasm-snip
+    pnpm rewriter:build
+    pnpm build
 fi
-
-pnpm rewriter:build
-pnpm build
