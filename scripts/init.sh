00e34d8041806dd2b4a9d22fd39127dd4426810b
lily <ilovesuno@proton.me>
2025-09-30 11:19:01 -0700

    Remove Rust patch

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-Panic="/home/codespace/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/core/src/panicking.rs"
-if [ -f "$Panic" ]; then
-    sed -i '/#\[cfg(feature = "panic_immediate_abort")\]/, /);/ s/^/\/\//' "$Panic"
-    echo "Patched $Panic"
-else
-    echo "$Panic does not exist, skipping patch"
-fi
 bash scripts/node.sh
 bash scripts/downloader.sh
 node source/override.js
